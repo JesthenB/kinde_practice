ceb30ce67d0edcf5c98e656649d1ccd47ad940c4
JesthenB <jesthenh@gmail.com>
2024-01-24 12:10:41 -0600

    initial commit

---
+++ src/app/api/protected/route.ts
@@ -0,0 +1,12 @@
+export async function GET() {
+  const { getUser, isAuthenticated } = getKindeServerSession();
+
+  if (!(await isAuthenticated())) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  const user = await getUser();
+  const data = { message: "Hello User", id: user.id };
+
+  return NextResponse.json({ data });
+}
